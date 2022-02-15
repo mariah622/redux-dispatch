23e16690342165ce59d042bb3ae63e8b8088f9af
mariah622 <mjustine622@gmail.com>
2022-02-15 10:19:33 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,7 @@
+let state = {count: 0};
+let action = {type: 'INCREASE_COUNT'}
+
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +11,17 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render(){
+  document.body.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = changeState(state,action)
+  render()
+}
+render()
+
+
+
 
 changeState(state, action)
